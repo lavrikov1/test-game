5c24bd431acd62ef62b2597bb93ba2f7ac5e65f1
lavrikov1 <98525139+lavrikov1@users.noreply.github.com>
2024-01-30 10:59:58 +0200

    Update game2.js

--- game2.js
+++ game2.js
@@ -2,7 +2,7 @@ function updateRectangleWidth(shotsFired, maxShots) {
     var rectangle = document.querySelector('.rectangleHp');
     if (rectangle) {
         var widthPercentage = 100 - (shotsFired / maxShots * 100);
-        widthPercentage = Math.max(0, Math.min(widthPercentage, 100)); // Ограничиваем диапазон от 0% до 100%
-        rectangle.style.width = `calc(${widthPercentage}% - 20px)`;
+        widthPercentage = Math.max(0, Math.min(widthPercentage, 100)); // Ограничиваю диапазон от 0% до 100%
+        rectangle.style.width = `calc(${widthPercentage}% - 0px)`;
     }
 }
